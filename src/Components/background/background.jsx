b2710ccfd92b46262ae6f701c858d1613a42695d
OmKarThakre <omkar01thakre@gmail.com>
2024-12-09 14:32:07 +0530

    background commit

--- src/Components/background/background.jsx
+++ src/Components/background/background.jsx
@@ -7,12 +7,20 @@ const Background = () => {
     <div className="relative">
       <div className="absolute inset-0">
         {/* First Image with Responsive margin */}
-        <div className="ml-[16%] md:ml-[20%] lg:ml-[64rem]">
-          <img src={img1} alt="Decorative background 1" className="w-full object-cover" />
+        <div className="ml-[40%] sm:ml-[25rem] lg:ml-[50rem] xl:ml-[60rem]">
+          <img
+            src={img1}
+            alt="Decorative background 1"
+            className="w-full object-cover"
+          />
         </div>
-        {/* Second Image with responsive size */}
-        <div className="w-[25rem] h-[4rem] md:w-[20rem] sm:h-[3rem]">
-          <img src={img2} alt="Decorative background 2" className="w-full h-auto object-contain" />
+        {/* Second Image with Responsive Size */}
+        <div className="mt-4">
+          <img
+            src={img2}
+            alt="Decorative background 2"
+            className="w-[50%] sm:w-[40%] md:w-[30%] lg:w-[20%] xl:w-[15%] h-auto  "
+          />
         </div>
       </div>
     </div>
