96db194ebaca86c44def29be8ce05817358dbb9d
OmKarThakre <omkar01thakre@gmail.com>
2024-12-13 11:16:10 +0530

    ui hero commit

--- src/Components/Hero/hero.jsx
+++ src/Components/Hero/hero.jsx
@@ -4,47 +4,44 @@ import img2 from '../../assets/Man1bg.png';
 
 const Hero = () => {
   return (
-    <div className="relative flex flex-col lg:flex-row items-center justify-center min-h-screen p-4 lg:p-8 space-y-6 lg:space-y-0 lg:space-x-8">
+    <div className="relative flex flex-col xl:flex-row items-center justify-center min-h-screen p-6 xl:p-12 space-y-2 xl:space-y-0">
 
       {/* Text Section */}
-      <div className="flex flex-col items-center text-center space-y-4 order-2 lg:order-none lg:w-1/2 px-4 sm:px-6 md:px-8 lg:px-0 ml-0 lg:ml-[5rem]">
-        <div>
-          <h1 className="text-[#6AB5E4] text-2xl sm:text-4xl md:text-3xl lg:text-3xl font-Press leading-tight">
-            Hello! I Am Omkar
-          </h1>
-        </div>
-        <div>
-          <button className="bg-[#6AB5E4] text-[#000000] py-2 px-4 sm:py-3 sm:px-6 rounded transition duration-300 hover:bg-blue-600">
-            Download Resume
-          </button>
-        </div>
+      <div className="flex flex-col items-center xl:items-start text-center xl:text-left space-y-2 order-2 xl:order-none xl:w-1/2 px-6 sm:px-8 md:px-12 lg:ml-0 xl:ml-[9rem]">
+        <h1 className="text-[#6AB5E4] text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-6xl font-Press leading-tight">
+          Hello! I Am Omkar
+        </h1>
+        <button className="bg-[#6AB5E4] text-[#000000] py-3 px-6 sm:py-4 sm:px-8 rounded transition duration-300 hover:bg-[#56A0D3]">
+          Download Resume
+        </button>
       </div>
 
       {/* Image Section */}
-      <div className="relative w-full flex justify-center order-1 lg:order-none lg:w-1/2">
-        {/* Background Image */}
-        <img
-          src={img2}
-          alt="Background"
-          className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[100%] sm:w-[85%] md:w-[75%] lg:w-[60%] -z-10"
-        />
-        {/* Foreground Image */}
-        <img
-          src={img1}
-          alt="Foreground"
-          className="relative w-[90%] sm:w-[70%] md:w-[70%] lg:w-[90%]"
-        />
+      <div className="relative w-full h-full flex justify-center xl:w-1/2 xl:px-0 sm:px-2 md:px-4">
+        <div className="relative">
+          <img
+            src={img2}
+            alt="Background"
+            className="absolute top-0 left-1/2 transform -translate-x-1/2 w-full sm:w-[80%] md:w-[90%] lg:w-[75%] xl:w-[100%] -z-5"
+          />
+          <img
+            src={img1}
+            alt="Foreground"
+            className="relative w-[90%] sm:w-[70%] md:w-[85%] lg:w-[95%] xl:w-[100%] mx-auto"
+          />
+        </div>
       </div>
 
       {/* Additional Information */}
-      <div className="flex flex-col items-center text-center space-y-4 order-3 xl:text-left lg:w-[80%] md:w-[70%] lg:px-6 px-4 sm:px-6 md:px-8">
-        <h2 className="text-[#DF6951] text-[1rem] sm:text-1xl md:text-3xl lg:text-3xl font-semibold font-Poppins uppercase text-center md:text-left leading-tight">
+      <div className="flex flex-col items-center xl:items-start text-center xl:text-left space-y-2 xl:w-[70%] order-3 xl:order-none px-6 sm:px-8 md:px-12">
+        <h2 className="text-[#DF6951] text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-semibold font-Poppins uppercase">
           I'm excited to showcase my work as a frontend developer and UI designer
         </h2>
-        <p className="text-[#B5C7C1] text-1xl sm:text-base md:text-lg text-center md:text-left leading-relaxed">
+        <p className="text-[#B5C7C1] text-base sm:text-lg md:text-xl xl:text-[1.2rem] leading-relaxed">
           I'm here to bring your digital ideas to life.
         </p>
       </div>
+
     </div>
   );
 };
