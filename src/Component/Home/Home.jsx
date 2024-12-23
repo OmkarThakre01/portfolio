4d1a921acaba64aac77c87b66abedd7db581735d
OmKarThakre <omkar01thakre@gmail.com>
2024-12-23 20:03:12 +0530

    Your commit message here

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,27 +1,29 @@
 import React from "react";
-import "../Home/Home.css"; // Import CSS with the abstract brush styling
+import Img from "../../assets/Img.png"
 import { GrDownload } from "react-icons/gr";
 
 const Home = () => {
   return (
     <section
       id="home"
-      className="flex  justify-center h-screen text-center px-4 "
+      className="flex justify-center items-center h-screen text-center px-4 bg-cover bg-center"
+      style={{
+        backgroundImage: `url(${Img})`, // Replace with actual path
+      }}
     >
-      <div className="flex flex-col items-center justify-center  mt-[-5rem] w-full max-w-4xl  text-[#d3a646] text-center">
+      <div className="flex flex-col items-center justify-center w-full max-w-4xl text-[#d3a646] text-center">
         <h1 className="text-4xl md:text-5xl font-bold mb-4">
-          Hello! I Am <span className="abstract-brush text-[#466cd3] font-Bebas">Omkar</span>
+          Hello! I Am{" "}
+          <span className="abstract-brush text-[#466cd3] font-Bebas">
+            Omkar
+          </span>
         </h1>
         <p className="font-semibold text-[#133644] mb-4 text-lg md:text-3xl w-full sm:w-[80%] md:w-[70%] lg:w-[70%]">
           I'M EXCITED TO SHOWCASE MY WORK AS A{" "}
-          <span className="abstract-brush">
-            FRONTEND 
-          </span> DEVELOPER AND {" "}
-          <span className="abstract-brush">
-          UI DESIGNER         
-           </span>
+          <span className="abstract-brush">FRONTEND</span> DEVELOPER AND{" "}
+          <span className="abstract-brush">UI DESIGNER</span>
         </p>
-        <p className=" md:text-md text-xl font font-bold text-blue-400 mb-8 w-full sm:w-[90%] md:w-[70%] lg:w-[60%]">
+        <p className="md:text-md text-xl font font-bold text-blue-400 mb-8 w-full sm:w-[90%] md:w-[70%] lg:w-[60%]">
           I'm here to bring your digital ideas to life.
         </p>
         <button className="bg-[#466cd3] flex items-center justify-center gap-3 text-white px-4 md:px-6 py-4 md:py-3 rounded-sm text-sm md:text-lg font-medium  transition">
